d8563c870b85c377a66fb24a0af9aa1d2e126975
Biswajit Biswas <jfbiswajit@gmail.com>
2022-04-16 18:07:07 +0600

    Add: Show message when their is no users"

--- src/components/UsersList/Users.js
+++ src/components/UsersList/Users.js
@@ -2,6 +2,10 @@ import Card from '../UI/Card';
 import classes from './Users.module.css';
 
 export default function Users({ users }) {
+  if (users.length < 1) {
+    return <Card>No users found!</Card>;
+  }
+  
   return (
     <Card>
       {users.map((user) => (
